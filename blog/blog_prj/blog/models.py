d3a2aaf2a8c0e3910099719e67a9bd686e365c0b
unknown <abdc5307@gmail.com>
2026-05-04 21:23:29 +0900

    [0504] 이정은

--- blog/blog_prj/blog/models.py
+++ blog/blog_prj/blog/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 from users.models import User
+import os
+from uuid import uuid4
+from django.utils import timezone
 
-# Create your models here.
+def upload_filepath(instance,filename):
+    today_str=timezone.now().strftime("%Y%m%d")
+    file_basename=os.path.basename(filename)
+    return f'{instance._meta.model_name}/{today_str}/{str(uuid4())}_{file_basename}'
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, unique = True)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length =50)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
     author = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name ="posts")
+    category = models.ManyToManyField(to=Category, through="PostCategory", related_name = "posts")
+    image = models.ImageField(upload_to=upload_filepath, blank=True)
+    video = models.FileField(upload_to=upload_filepath, blank=True)
+    like = models.ManyToManyField(to=User, through="Like", related_name="like_posts")
 
     def __str__(self):
         return f'[{self.id}] self.title'
@@ -19,3 +36,11 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'[{self.id}] {self.content}'
+    
+class PostCategory(models.Model):
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE, related_name="post_categories")
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name="post_categories")
+
+class Like(models.Model):
+    user =models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="user_likes")
+    post= models.ForeignKey(to=Post, on_delete=models.CASCADE, related_name="post_likes")
